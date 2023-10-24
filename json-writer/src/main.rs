fb3ce8bc7b03de5aa1c112114c51e34c755364b7
Mihir Kumar Singh <mihir39nr@gmail.com>
2023-10-24 19:32:46 +0530

    JSON Writer Completed

--- json-writer/src/main.rs
+++ json-writer/src/main.rs
@@ -1,3 +1,32 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize,Deserialize)]
+struct Paragraph {
+    name: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct Article{
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>
+}
 fn main() {
-    println!("Hello, world!");
+    let article = Article{
+        article: String::from("Working With JSON in Rust"),
+        author: String::from("Mihir"),
+        paragraph: vec![
+            Paragraph{
+                name:String::from("First Sentence")
+            },
+            Paragraph{
+                name:String::from("Body pf the Paragraph")
+            },
+            Paragraph{
+                name:String::from("End of the Paragraph.")
+            }
+        ]
+    };
+    let json = serde_json::to_string(&article).unwrap();
+    println!("JSON is {}",json)
 }
